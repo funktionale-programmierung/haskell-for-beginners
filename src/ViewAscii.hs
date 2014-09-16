de30e62c2a33b1a4aff826ab3d048b45db978368
Stefan Wehr <mail@stefanwehr.de>
2014-09-16 12:23:29 +0200

    improvements to view-ascii

--- src/ViewAscii.hs
+++ src/ViewAscii.hs
@@ -1,34 +1,32 @@
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Control.Exception
-import Data.Word
 import System.IO
 import System.Environment
 import System.Exit
 
-_SPECIAL_CODES_ :: [(BS.ByteString, BS.ByteString)]
+_SPECIAL_CODES_ :: [(BS.ByteString, String)]
 _SPECIAL_CODES_ =
-    [(p1 0xE4      , BSC.pack "<iso-8859-1: a with diaresis>")
-    ,(p1 0xF6      , BSC.pack "<iso-8859-1: o with diaresis>")
-    ,(p1 0xFC      , BSC.pack "<iso-8859-1: u with diaresis>")
-    ,(p1 0xC4      , BSC.pack "<iso-8859-1: A with diaresis>")
-    ,(p1 0xD6      , BSC.pack "<iso-8859-1: O with diaresis>")
-    ,(p1 0xDC      , BSC.pack "<iso-8859-1: U with diaresis>")
-    ,(p1 0xDF      , BSC.pack "<iso-8859-1: sharp s>")
-    ,(p2 0xC3 0xA4 , BSC.pack "<utf-8: a with diaresis>")
-    ,(p2 0xC3 0xB6 , BSC.pack "<utf-8: o with diaresis>")
-    ,(p2 0xC3 0xBC , BSC.pack "<utf-8: u with diaresis>")
-    ,(p2 0xC3 0x84 , BSC.pack "<utf-8: A with diaresis>")
-    ,(p2 0xC3 0x96 , BSC.pack "<utf-8: O with diaresis>")
-    ,(p2 0xC3 0x9C , BSC.pack "<utf-8: U with diaresis>")
-    ,(p2 0xC3 0x9f , BSC.pack "<utf-8: sharp s>")]
+    [(p1 0xE4      , "<iso-8859-1: a with diaresis>")
+    ,(p1 0xF6      , "<iso-8859-1: o with diaresis>")
+    ,(p1 0xFC      , "<iso-8859-1: u with diaresis>")
+    ,(p1 0xC4      , "<iso-8859-1: A with diaresis>")
+    ,(p1 0xD6      , "<iso-8859-1: O with diaresis>")
+    ,(p1 0xDC      , "<iso-8859-1: U with diaresis>")
+    ,(p1 0xDF      , "<iso-8859-1: sharp s>")
+    ,(p2 0xC3 0xA4 , "<utf-8: a with diaresis>")
+    ,(p2 0xC3 0xB6 , "<utf-8: o with diaresis>")
+    ,(p2 0xC3 0xBC , "<utf-8: u with diaresis>")
+    ,(p2 0xC3 0x84 , "<utf-8: A with diaresis>")
+    ,(p2 0xC3 0x96 , "<utf-8: O with diaresis>")
+    ,(p2 0xC3 0x9C , "<utf-8: U with diaresis>")
+    ,(p2 0xC3 0x9f , "<utf-8: sharp s>")]
     where
-      p1 :: Word8 -> BS.ByteString
       p1 = BS.singleton
-      p2 :: Word8 -> Word8 -> BS.ByteString
       p2 w1 w2 = BS.pack [w1, w2]
 
-lookupSpecial :: BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
+lookupSpecial :: BS.ByteString -> Maybe (String, BS.ByteString)
 lookupSpecial bs =
     lookup _SPECIAL_CODES_
     where
@@ -52,10 +50,13 @@ transform chunk acc =
                    let newAcc' =
                            (if b == 10
                             then BS.singleton b -- \n
-                            else BSC.pack ("<" ++ show b ++ ">")) : newAcc
+                            else encode ("<" ++ show b ++ ">")) : newAcc
                    in transform rest newAcc'
          Just (special, rest) ->
-             transform rest (special : newAcc)
+             transform rest (encode special : newAcc)
+    where
+      encode :: String -> BS.ByteString
+      encode s = T.encodeUtf8 (T.pack s)
 
 handleFile :: FilePath -> IO ()
 handleFile file =
@@ -72,18 +73,13 @@ handleFile file =
              else do BS.putStr (transform bs [])
                      loop h
 
-data ViewAsciiOpts
-    = ViewAsciiOpts
-      { to_files :: [FilePath]
-      }
-
-parseArgs :: [String] -> IO ViewAsciiOpts
+parseArgs :: [String] -> IO [FilePath]
 parseArgs args =
     if "-h" `elem` args || "--help" `elem` args
     then usage
     else if null args
-         then return (ViewAsciiOpts ["-"])
-         else return (ViewAsciiOpts args)
+         then return ["-"]
+         else return args
     where
       usage = abort "USAGE: view-ascii [FILE ...]"
       abort msg =
@@ -92,5 +88,5 @@ parseArgs args =
 
 main =
     do args <- getArgs
-       opts <- parseArgs args
-       mapM_ handleFile (to_files opts)
+       files <- parseArgs args
+       mapM_ handleFile files
